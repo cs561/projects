a57b1d5e47ee3de6c4c87a2d74d269c594e4569b
Michael Schneider <buschviper@gmail.com>
2013-11-30 01:37:48 +0100

    fixed canvas pixel ratio

--- Build your own Cloud II/Raytrace Projekt/RaytraceClient/web/js/offline.js
+++ Build your own Cloud II/Raytrace Projekt/RaytraceClient/web/js/offline.js
@@ -1,32 +1,30 @@
 ﻿function testCanvas() {
-    //alert("Test");
-    var canvas = document.getElementById("frameBuffer");
-    var cWidth = canvas.width;
-    var cHeight = canvas.height;
-    var ctx = canvas.getContext("2d");
+    var cWidth = $("#frameBuffer").width();
+    var cHeight = $("#frameBuffer").height();
+    $('#frameBuffer').attr({ width: cWidth, height: cHeight });
+    var ctx = $("#frameBuffer")[0].getContext("2d");
     var imageData = ctx.getImageData(0, 0, cWidth, cHeight);
     var data = imageData.data;
-
     for (var y = 0; y < cHeight; ++y) {
+        
         for (var x = 0; x < cWidth; ++x) {
             var index = (y * cWidth + x) * 4;
             var value = x * y & 0xff;
-            data[index] = value;	// red
-            data[++index] = value;	// green
-            data[++index] = value;	// blue
-            data[++index] = 255;	// alpha
-            
+
+                data[index] = value;	// red
+                data[++index] = value;	// green
+                data[++index] = value;	// blue
+                data[++index] = 255;	// alpha
         }
     }
+
     ctx.putImageData(imageData, 0, 0);
 }
 
 function setCanvas() {
-    var canvas = document.getElementById("frameBuffer");
-    var ctx = canvas.getContext("2d");
 
-    canvas.style.width = document.getElementById("canvasWidth").value + "px";
-    canvas.style.height = document.getElementById("canvasHeight").value + "px";
+    $("#frameBuffer").width($("#canvasWidth").val() + "px");
+    $("#frameBuffer").height($("#canvasHeight").val() + "px");
     scrollToTop();
 }
 
@@ -36,4 +34,9 @@ function scrollToTop() {
         timeOut = setTimeout('scrollToTop()', 10);
     }
     else clearTimeout(timeOut);
+}
+
+function WebSocket() {
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", "");
 }
